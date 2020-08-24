dfef2d3c864a9f4a3c9e65318fc27320b0f5498b
Riva Quiroga <riva.quiroga@uc.cl>
2020-08-24 19:24:01 -0400

    fix typo in region name

--- tests/testthat/test-estandariza_regiones.R
+++ tests/testthat/test-estandariza_regiones.R
@@ -2,7 +2,7 @@ test_that("multiplication works", {
 
   some_regions <- c("Aisen", "biobio", "R.M.")
   some_other_regions <- c("Aysén", "Bío-Bío", "Metropolitana")
-  standard_names <- c("Aysén del General Carlos Ibañez del Campo", "Biobío", "Metropolitana de Santiago")
+  standard_names <- c("Aysén del General Carlos Ibáñez del Campo", "Biobío", "Metropolitana de Santiago")
 
   expect_equal(estandariza_regiones(some_regions), standard_names)
   expect_equal(estandariza_regiones(some_other_regions), standard_names)
